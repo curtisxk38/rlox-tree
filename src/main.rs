aac392698b08779f2888ee03707ff2da6fd52055
curtisxk38 <ck2ha@virginia.edu>
2020-05-15 20:15:46 -0400

    Create interpreter struct

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,60 @@ use std::env;
 use std::fs;
 use std::io;
 
-fn run_file(filename: &String) {
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    run(&contents);
+mod scan;
+
+struct Interpreter {
+    had_error: bool
 }
 
-fn run_prompt() {
-    println!("Hello repl");
-    let mut input = String::new();
-    
-    loop {
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => { 
-                run(&input);
-                input = String::new();
+impl Interpreter {
+
+    pub fn new() -> Interpreter {
+        return Interpreter { had_error: false};
+    }
+
+    fn run_file(&self, filename: &String) {
+        let contents = fs::read_to_string(filename)
+            .expect("Something went wrong reading the file");
+        self.run(&contents);
+    }
+
+    fn run_prompt(&self) {
+        println!("Hello repl");
+        let mut input = String::new();
+        
+        loop {
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => { 
+                    self.run(&input);
+                    input = String::new();
+                }
+                Err(error) => println!("error: {}", error),
             }
-            Err(error) => println!("error: {}", error),
         }
     }
-}
 
-fn run(input: &String) {
-    println!("{}", input);
-}
+    fn run(&self, input: &String) {
+        println!("{}", input);
+    }
+
+    fn error(&mut self, line: i32, error_where: &String, message: &String) {
+        println!("[line {} ] Error{}: {}", line, error_where, message);
+        self.had_error = true;
+    }
+    
+}                                                 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let lox = Interpreter::new();
     if args.len() > 2 {
         panic!("usage: rlox [script]");
     }
     else if args.len() == 2 {
-        run_file(&args[1]);
+        lox.run_file(&args[1]);
     }
     else {
-        run_prompt();
+        lox.run_prompt();
     }
 }
