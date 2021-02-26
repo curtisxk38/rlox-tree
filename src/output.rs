3263c145a2e2f4b6811d0d703dcafe1e45698a99
curtisxk38 <ck2ha@virginia.edu>
2021-02-26 18:12:58 -0500

    Stop inaccurate dead code warning

--- src/output.rs
+++ src/output.rs
@@ -6,6 +6,10 @@ pub(crate) struct Printer {
 
 }
 
+// We're allowing dead code, so that warnings aren't generated
+// however the code isn't actually dead. I guess rustc/rust-analyzer can't tell that
+// because we're using conditional compilation. (see tree_walker.rs)
+#[allow(dead_code)]
 impl Printer {
     pub fn new() -> Printer {
         Printer{}
@@ -22,6 +26,7 @@ pub(crate) struct Recorder {
     pub outputted: Vec<String>
 }
 
+#[allow(dead_code)]
 impl Recorder {
     pub fn new() -> Recorder {
         Recorder {outputted: Vec::new()}
